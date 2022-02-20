a4468c437558cf1cccb56bd254e1bedd078c982a
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-02-19 18:24:32 -0700

    added inquirer prompt

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
 // allows the app.js file to access the fs module's functions through the fs assignment
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-// 2 is to make a new args aside from the original args of the file. Kind of like another branch from the main.
-// slice a brand-new array based on process.argv(what's inputed after node app.js)
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// First argument is the file created, or the output file. The second argument is the data that's being written: The HTML string template. The third argument is the callback function that will handle any error as well as the success message.
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    // If an error exist, an error message is displayed using the following statement:
-    if (err) throw err;
-    // logged if ran successfully
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-})
+// // First argument is the file created, or the output file. The second argument is the data that's being written: The HTML string template. The third argument is the callback function that will handle any error as well as the success message.
+// fs.writeFile('./index.html', pageHTML, err => {
+//     // If an error exist, an error message is displayed using the following statement:
+//     if (err) throw err;
+//     // logged if ran successfully
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// })
+
+
+
 
 // const printProfileData = profileDataArr => {
 //     // this...
